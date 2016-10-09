dd3bd3425acdf4ff4cdb90396ee4955fc77996f3
vktharikonda <vinaytharikonda@gmail.com>
2016-10-08 20:40:35 -0500

    Add files via upload

---
+++ src/exports/ExportTXT.java
@@ -0,0 +1,14 @@
+package com.post.exports;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExportTXT extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException
+	{
+		
+	}
+}
